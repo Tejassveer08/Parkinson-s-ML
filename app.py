8452885cd456ec85179110f9ed369d39af26aeb1
Tejassveer Vasant <147965837+Tejassveer08@users.noreply.github.com>
2025-03-15 11:35:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ X_pca = pca.fit_transform(X_scaled)
 # Streamlit UI
 st.title("Parkinson's Disease Prediction")
 st.write("This app predicts Parkinson's Disease based on patient voice measures.")
+st.write("Attribute Information : Matrix column entries (attributes): ***name*** - ASCII subject name and recording number ***MDVP:Fo(Hz)***- Average vocal fundamental frequency ***MDVP:Fhi(Hz)*** - Maximum vocal fundamental frequency **MDVP:Flo(Hz)** - Minimum vocal fundamental frequency ***MDVP:Jitter(%), MDVP:Jitter(Abs), MDVP:RAP, MDVP:PPQ, Jitter:DDP*** - Several measures of variation in fundamental frequency ***MDVP:Shimmer, MDVP:Shimmer(dB), Shimmer:APQ3, Shimmer:APQ5, MDVP:APQ, Shimmer:DDA*** - Several measures of variation in amplitude ***NHR,HNR***- Two measures of ratio of noise to tonal components in the voice **status** - Health status of the subject (one) - Parkinson's, (zero) - healthy (Dependent variable.) ***RPDE,D2*** - Two nonlinear dynamical complexity measures ***DFA*** - Signal fractal scaling exponent ***spread1,spread2,PPE*** - Three nonlinear measures of fundamental frequency variation")
 
 # Show raw data
 if st.checkbox("Show Raw Data"):
